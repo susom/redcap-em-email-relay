b4faf2ebb0d5064bbd43b65b85ebfd76ddf1fc57
Irvin Szeto <irvins@stanford.edu>
2018-10-15 13:41:58 -0700

    reenabling emlogger

--- emLoggerTrait.php
+++ emLoggerTrait.php
@@ -6,20 +6,20 @@ trait emLoggerTrait
 
     function emLog() {
         $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-        // $emLogger->emLog($this->PREFIX, func_get_args(), "INFO");
+        $emLogger->emLog($this->PREFIX, func_get_args(), "INFO");
     }
 
     function emDebug() {
         // Check if debug enabled
         if ( $this->getSystemSetting('enable-system-debug-logging') || ( !empty($_GET['pid']) && $this->getProjectSetting('enable-project-debug-logging'))) {
             $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-            // $emLogger->emLog($this->PREFIX, func_get_args(), "DEBUG");
+            $emLogger->emLog($this->PREFIX, func_get_args(), "DEBUG");
         }
     }
 
     function emError() {
         $emLogger = \ExternalModules\ExternalModules::getModuleInstance('em_logger');
-        // $emLogger->emLog($this->PREFIX, func_get_args(), "ERROR");
+        $emLogger->emLog($this->PREFIX, func_get_args(), "ERROR");
     }
 
 }
